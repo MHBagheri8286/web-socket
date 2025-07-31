6fc693c79e3ac970ae2128918fd52d9bd23ee07c
Mohammadhassan <bagheri.mh69@gmail.com>
2025-07-31 19:23:57 +0200

    conf: Add jest configs to project

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
-    testMatch: ['<rootDir>/tests/**/*.test.{js,jsx}'],
-    moduleNameMapping: {
-      '\\.(css|less|scss)$': 'identity-obj-proxy',
-    },
-    collectCoverageFrom: [
-      'src/**/*.{js,jsx}',
-      '!src/index.jsx'
-    ]
-  };
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
+  testMatch: ['<rootDir>/tests/**/*.test.{js,jsx}'],
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          '@babel/preset-env',
+          ['@babel/preset-react', { runtime: 'automatic' }],
+        ],
+      },
+    ],
+  },
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/index.jsx'],
+};
